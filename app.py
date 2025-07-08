42bdbf5b569639836b7a8850bcc29b36df443be2
Arjun Upadhyayula <arjun.upadhyayula@csedge.org>
2025-07-08 12:02:18 -0700

    commit

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("this is my first Repo!!")
+
+adjective = input("Enter an adjective: ")
+plural_noun= input("Enter a plural noun: ")
+noun= input("Enter a noun: ")
+relative = input("Enter your relative: ")
+adjective2 = input("Enter another adjective: ")
+plural_noun2 = input("Enter another plural noun: ")
+adjective3= input("Enter another adjective: ")
+noun2= input("Enter another noun: ")
+adjective4 = input("Enter an adjective: ")
+noun3 = input("Enter another noun: ")
+relative2 = input("Enter your relative: ")
+
+print("One " + adjective + " morning, the " + plural_noun + " went to the " + noun + " to do their weekly shopping. As they walked down the aisles, they bumped into their " + relative + " who was wearing a hilarious " + adjective2 + " costume. Suddenly, a group of " + plural_noun2 + " started singing karaoke in the produce section, much to the surprise of the shoppers. Trying to navigate the chaos, they accidentally knocked over a display of " + adjective3 + " " + noun2 + ", causing a domino effect throughout the store. The manager, a " + adjective4 + " " + noun3 + ", came rushing over to assess the situation and ended up getting caught in a silly string fight with the mischievous " + relative2 + ". In the end, everyone couldn't help but laugh at the ridiculousness of their unexpected trip to the supermarket.")
